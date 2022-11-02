0b3a5e8c4b1a6321f6d550127b228ecd3ac90679
xikest <xikest12@gmail.com>
2022-11-03 01:33:22 +0900

    Update data.py

--- data_provider/data.py
+++ data_provider/data.py
@@ -89,13 +89,13 @@ class Data:
                     print(f'{symbol},prices: {e}')
                     pass
                 
-        class FamaFrench:
-            @staticmethod
-            def load_from_web(symbols:List[str]=['F-F_Research_Data_5_Factors_2x3'], start_date='2020-1-1', end_date='2022-12-31') -> pd.DataFrame:
-                for symbol in symbols: 
-                        try:
-                            df = web.DataReader(symbol, 'famafrench', start=start_date, end=end_date)
-                            yield df[0]
-                        except Exception as e:
-                            print(f'F-F_Research_Data: {e}')
-                            pass
+    class FamaFrench:
+        @staticmethod
+        def load_from_web(symbols:List[str]=['F-F_Research_Data_5_Factors_2x3'], start_date='2020-1-1', end_date='2022-12-31') -> pd.DataFrame:
+            for symbol in symbols: 
+                    try:
+                        df = web.DataReader(symbol, 'famafrench', start=start_date, end=end_date)
+                        yield df[0]
+                    except Exception as e:
+                        print(f'F-F_Research_Data: {e}')
+                        pass
