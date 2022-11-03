8bfd738adc23b82e0ffa83ef6b2f69efba925fa9
xikest <xikest12@gmail.com>
2022-11-03 20:06:26 +0900

    Update data.py

--- data_provider/data.py
+++ data_provider/data.py
@@ -22,7 +22,7 @@ class Data:
         #                     print(f'{symbol},prices: {e}')
         #                     pass
         @staticmethod
-        def load_from_web(symbols:List[str]=['AAPL'], start_date='2020-1-1', end_date='2022-12-31', columns_data = ['Adj Close']) -> pd.DataFrame:
+        def load_from_web(symbols:List[str]=['AAPL'], start_date='2020-1-1', end_date='2022-12-31') -> pd.DataFrame:
             """
             pandaDatareader에서 데이터를 받아온다.
             
@@ -30,7 +30,6 @@ class Data:
                 symbol (str): 주식의 심볼
                 start_date (_type_): 조회 시작일
                 end_date (_type_): 조회 종료일
-                columns_data = ['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close']
             Yields:
                 pd.DataFrame: 요청된 symbols의 adj close 데이터를 반환한다. 
                 
@@ -50,7 +49,7 @@ class Data:
                 try:
                     df = web.DataReader(symbol, 'yahoo', start=start_date, end=end_date)
                     df['Symbol']=symbol
-                    yield  df.reset_index().set_index(['Date','Symbol']).loc[:,columns_data] #yahoo: 'Adj Close'
+                    yield  df.reset_index().set_index(['Date','Symbol']).loc[:,['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close']] #yahoo: 'Adj Close'
                 except Exception as e:
                     print(f'{symbol},prices: {e}')
                     pass
@@ -71,7 +70,7 @@ class Data:
                             
 
         @staticmethod
-        def load_from_fdr(symbols:List[str]=['AAPL'], start_date='2020-1-1', end_date='2022-12-31', columns_data = ['Adj Close']) -> pd.DataFrame:
+        def load_from_fdr(symbols:List[str]=['AAPL'], start_date='2020-1-1', end_date='2022-12-31') -> pd.DataFrame:
             """
             FinacialDatareader에서 데이터를 받아온다.
             Args:
@@ -79,7 +78,6 @@ class Data:
                         source (str): 데이터의 소스: yahoo와 FF 데이터_
                         start_date (_type_): 조회 시작일
                         end_date (_type_): 조회 종료일
-                        columns_data = ['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close']
             Yields:
                         pd.DataFrame: 요청된 symbols의 adj close 데이터를 반환한다. 
             """
@@ -87,7 +85,7 @@ class Data:
                 try:
                     df = fdr.DataReader(symbol, start=start_date, end=end_date)
                     df['Symbol']=symbol
-                    yield  df.reset_index().set_index(['Date','Symbol']).loc[:,columns_data] #yahoo: 'Adj Close'
+                    yield  df.reset_index().set_index(['Date','Symbol']).loc[:,['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close']] #yahoo: 'Adj Close'
                 except Exception as e:
                     print(f'{symbol},prices: {e}')
                     pass
